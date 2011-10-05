3d98cbb513c8f9516809a612405630f50e06f88e
Mike Norman <michael.norman@oracle.com>
2011-10-05 15:13:00 -0400

    Finish up unit tests for deeply nested types

--- java/src/org/eclipse/persistence/tools/oracleddl/test/ddlparser/PackageDDLTestSuite.java
+++ java/src/org/eclipse/persistence/tools/oracleddl/test/ddlparser/PackageDDLTestSuite.java
@@ -23,19 +23,25 @@ import java.util.List;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
 //DDL imports
+import org.eclipse.persistence.tools.oracleddl.metadata.DatabaseType;
 import org.eclipse.persistence.tools.oracleddl.metadata.FieldType;
+import org.eclipse.persistence.tools.oracleddl.metadata.NumericType;
+import org.eclipse.persistence.tools.oracleddl.metadata.PLSQLCollectionType;
 import org.eclipse.persistence.tools.oracleddl.metadata.PLSQLPackageType;
 import org.eclipse.persistence.tools.oracleddl.metadata.PLSQLRecordType;
 import org.eclipse.persistence.tools.oracleddl.metadata.PLSQLType;
+import org.eclipse.persistence.tools.oracleddl.metadata.SizedType;
+import org.eclipse.persistence.tools.oracleddl.metadata.VarChar2Type;
 import org.eclipse.persistence.tools.oracleddl.parser.DDLParser;
 import org.eclipse.persistence.tools.oracleddl.parser.ParseException;
 import org.eclipse.persistence.tools.oracleddl.util.DatabaseTypesRepository;
-
 
 public class PackageDDLTestSuite {
 
@@ -53,6 +59,10 @@ public class PackageDDLTestSuite {
         parser.setTypesRepository(new DatabaseTypesRepository());
     }
 
+    /*
+    CREATE PACKAGE EMPTY_PACKAGE AS
+    END EMPTY_PACKAGE;
+    */
     static final String EMPTY_PACKAGE = "EMPTY_PACKAGE";
     static final String CREATE_EMPTY_PACKAGE =
         CREATE_PACKAGE_PREFIX + EMPTY_PACKAGE + " AS END " + EMPTY_PACKAGE + ";";
@@ -237,6 +247,13 @@ public class PackageDDLTestSuite {
             packageType.getTypes().size());
         PLSQLType t1 = packageType.getTypes().get(0);
         assertEquals("collection type1 has wrong name", PACKAGE_WCOLLECTION_NAME, t1.getTypeName());
+        PLSQLCollectionType collType = (PLSQLCollectionType)t1;
+        assertTrue("nestedType should be associative", collType.isIndexed());
+        DatabaseType nestedType = collType.getNestedType();
+        assertEquals("nestedType has wrong name", new VarChar2Type().getTypeName(),
+            nestedType.getTypeName());
+        VarChar2Type varcharNestedType = (VarChar2Type)nestedType;
+        assertEquals("nestedType has wrong size", 20L, varcharNestedType.getSize());
     }
 
     /*
@@ -261,17 +278,22 @@ public class PackageDDLTestSuite {
     static final String PACKAGE_WCOLLECTION_WRECORD_TYPE1_FIELD4_TYPE = "VARCHAR2";
     static final String PACKAGE_WCOLLECTION_WRECORD_TYPE1_FIELD5_NAME = "PUBLISHED_ON";
     static final String PACKAGE_WCOLLECTION_WRECORD_TYPE1_FIELD5_TYPE = "DATE";
-    static final String PACKAGE_WCOLLECTION_WRECORD_NAME = "BOOKS_TAB";
+    static final String PACKAGE_WCOLLECTION_WRECORD_TYPE2 = "BOOKS_TAB";
     static final String CREATE_PACKAGE_WCOLLECTION_WRECORD =
         CREATE_PACKAGE_PREFIX + PACKAGE_WCOLLECTION_WRECORD + " AS " +
             "\nTYPE " + PACKAGE_WCOLLECTION_WRECORD_TYPE1 + " IS RECORD (" +
-                "\n" + PACKAGE_WCOLLECTION_WRECORD_TYPE1_FIELD1_NAME + " " + PACKAGE_WCOLLECTION_WRECORD_TYPE1_FIELD1_TYPE + "," +
-                "\n" + PACKAGE_WCOLLECTION_WRECORD_TYPE1_FIELD2_NAME + " " + PACKAGE_WCOLLECTION_WRECORD_TYPE1_FIELD2_TYPE + "," +
-                "\n" + PACKAGE_WCOLLECTION_WRECORD_TYPE1_FIELD3_NAME + " " + PACKAGE_WCOLLECTION_WRECORD_TYPE1_FIELD3_TYPE + "," +
-                "\n" + PACKAGE_WCOLLECTION_WRECORD_TYPE1_FIELD4_NAME + " " + PACKAGE_WCOLLECTION_WRECORD_TYPE1_FIELD4_TYPE + "," +
-                "\n" + PACKAGE_WCOLLECTION_WRECORD_TYPE1_FIELD5_NAME + " " + PACKAGE_WCOLLECTION_WRECORD_TYPE1_FIELD5_TYPE +
+                "\n" + PACKAGE_WCOLLECTION_WRECORD_TYPE1_FIELD1_NAME + " " +
+                    PACKAGE_WCOLLECTION_WRECORD_TYPE1_FIELD1_TYPE + "," +
+                "\n" + PACKAGE_WCOLLECTION_WRECORD_TYPE1_FIELD2_NAME + " " +
+                    PACKAGE_WCOLLECTION_WRECORD_TYPE1_FIELD2_TYPE + "," +
+                "\n" + PACKAGE_WCOLLECTION_WRECORD_TYPE1_FIELD3_NAME + " " +
+                    PACKAGE_WCOLLECTION_WRECORD_TYPE1_FIELD3_TYPE + "," +
+                "\n" + PACKAGE_WCOLLECTION_WRECORD_TYPE1_FIELD4_NAME + " " +
+                    PACKAGE_WCOLLECTION_WRECORD_TYPE1_FIELD4_TYPE + "," +
+                "\n" + PACKAGE_WCOLLECTION_WRECORD_TYPE1_FIELD5_NAME + " " +
+                    PACKAGE_WCOLLECTION_WRECORD_TYPE1_FIELD5_TYPE +
             "\n);" +
-            "\nTYPE " + PACKAGE_WCOLLECTION_WRECORD_NAME + " IS TABLE OF " +
+            "\nTYPE " + PACKAGE_WCOLLECTION_WRECORD_TYPE2 + " IS TABLE OF " +
                 PACKAGE_WCOLLECTION_WRECORD_TYPE1 + ";" +
         "\nEND " + PACKAGE_WCOLLECTION_WRECORD + ";";
     @Test
@@ -285,7 +307,174 @@ public class PackageDDLTestSuite {
         catch (ParseException pe) {
             worked = false;
         }
-        assertTrue("package with collection with record should parse", worked);
+        assertTrue("package with collection containing record should parse", worked);
+        assertEquals("package with collection containing record has wrong name",
+            packageType.getPackageName(), PACKAGE_WCOLLECTION_WRECORD);
+        assertNull("package with collection containing record should have no procedures",
+            packageType.getProcedures());
+        assertNotNull("package with collection containing record should have types",
+            packageType.getTypes());
+        assertEquals("package with collection containing record should have exactly 2 types", 2,
+            packageType.getTypes().size());
+        PLSQLType t1 = packageType.getTypes().get(0);
+        assertEquals("package with collection containing record type1 has wrong name",
+            PACKAGE_WCOLLECTION_WRECORD_TYPE1, t1.getTypeName());
+        PLSQLRecordType t1Rec = (PLSQLRecordType)t1;
+        List<FieldType> fields = t1Rec.getFields();
+        assertEquals("package with collection containing record type1 field1 has wrong name",
+            PACKAGE_WCOLLECTION_WRECORD_TYPE1_FIELD1_NAME, fields.get(0).getFieldName());
+        assertEquals("package with collection containing record type1 field1 has wrong type",
+            new VarChar2Type().getTypeName(), fields.get(0).getTypeName());
+        assertEquals("package with collection containing record type1 field1 has wrong size",
+            20L, ((SizedType)fields.get(0).getDataType()).getSize());
+        assertEquals("package with collection containing record type1 field2 has wrong name",
+            PACKAGE_WCOLLECTION_WRECORD_TYPE1_FIELD2_NAME, fields.get(1).getFieldName());
+        assertEquals("package with collection containing record type1 field2 has wrong type",
+            new VarChar2Type().getTypeName(), fields.get(1).getTypeName());
+        assertEquals("package with collection containing record type1 field2 has wrong size",
+            2000L, ((SizedType)fields.get(1).getDataType()).getSize());
+        assertEquals("package with collection containing record type1 field3 has wrong name",
+            PACKAGE_WCOLLECTION_WRECORD_TYPE1_FIELD3_NAME, fields.get(2).getFieldName());
+        assertEquals("package with collection containing record type1 field3 has wrong type",
+            new VarChar2Type().getTypeName(), fields.get(2).getTypeName());
+        assertEquals("package with collection containing record type1 field3 has wrong size",
+            2000L, ((SizedType)fields.get(2).getDataType()).getSize());
+        assertEquals("package with collection containing record type1 field4 has wrong name",
+            PACKAGE_WCOLLECTION_WRECORD_TYPE1_FIELD4_NAME, fields.get(3).getFieldName());
+        assertEquals("package with collection containing record type1 field4 has wrong type",
+            PACKAGE_WCOLLECTION_WRECORD_TYPE1_FIELD4_TYPE, fields.get(3).getTypeName());
+        assertEquals("package with collection containing record type1 field5 has wrong name",
+            PACKAGE_WCOLLECTION_WRECORD_TYPE1_FIELD5_NAME, fields.get(4).getFieldName());
+        assertEquals("package with collection containing record type1 field5 has wrong type",
+            PACKAGE_WCOLLECTION_WRECORD_TYPE1_FIELD5_TYPE, fields.get(4).getTypeName());
+        PLSQLType t2 = packageType.getTypes().get(1);
+        assertEquals("package with collection containing record type2 has wrong name",
+            PACKAGE_WCOLLECTION_WRECORD_TYPE2, t2.getTypeName());
+        PLSQLCollectionType collT2 = (PLSQLCollectionType)t2;
+        assertFalse("package with collection containing record type2 nestedType should " +
+        		"not be associative", collT2.isIndexed());
+        DatabaseType nestedType = collT2.getNestedType();
+        assertEquals("package with collection containing record type2 nestedType has wrong name",
+            PACKAGE_WCOLLECTION_WRECORD_TYPE1, nestedType.getTypeName());
+        assertSame("package with collection containing record type2 nestedType wrong instance",
+            nestedType, t1Rec);
+    }
 
+    /*
+     TYPE TBL1 IS TABLE OF VARCHAR2(111) INDEX BY BINARY_INTEGER;
+     TYPE TBL2 IS TABLE OF NUMBER INDEX BY BINARY_INTEGER;
+     TYPE ARECORD IS RECORD (
+        T1 TBL1,
+        T2 TBL2,
+        T3 BOOLEAN
+     );
+     TYPE TBL3 IS TABLE OF ARECORD INDEX BY PLS_INTEGER;
+     TYPE TBL4 IS TABLE OF TBL2 INDEX BY PLS_INTEGER;
+    */
+    static final String PACKAGE_WDEEPLY_NESTED_TYPES = "PACKAGE_WDEEPLY_NESTED_TYPES";
+    static final String PACKAGE_WDEEPLY_NESTED_TYPE1 = "TBL1";
+    static final String PACKAGE_WDEEPLY_NESTED_TYPE1_NESTED_TYPE = "VARCHAR2(111)";
+    static final String PACKAGE_WDEEPLY_NESTED_TYPE1_INDEX_TYPE = "BINARY_INTEGER";
+    static final String PACKAGE_WDEEPLY_NESTED_TYPE2 = "TBL2";
+    static final String PACKAGE_WDEEPLY_NESTED_TYPE2_NESTED_TYPE = "NUMBER";
+    static final String PACKAGE_WDEEPLY_NESTED_TYPE2_INDEX_TYPE =
+        PACKAGE_WDEEPLY_NESTED_TYPE1_INDEX_TYPE;
+    static final String PACKAGE_WDEEPLY_NESTED_TYPE3 = "ARECORD";
+    static final String PACKAGE_WDEEPLY_NESTED_TYPE3_FIELD1_NAME = "T1";
+    static final String PACKAGE_WDEEPLY_NESTED_TYPE3_FIELD1_TYPE = PACKAGE_WDEEPLY_NESTED_TYPE1;
+    static final String PACKAGE_WDEEPLY_NESTED_TYPE3_FIELD2_NAME = "T2";
+    static final String PACKAGE_WDEEPLY_NESTED_TYPE3_FIELD2_TYPE = PACKAGE_WDEEPLY_NESTED_TYPE2;
+    static final String PACKAGE_WDEEPLY_NESTED_TYPE3_FIELD3_NAME = "T3";
+    static final String PACKAGE_WDEEPLY_NESTED_TYPE3_FIELD3_TYPE = "BOOLEAN";
+    static final String PACKAGE_WDEEPLY_NESTED_TYPE4 = "TBL3";
+    static final String PACKAGE_WDEEPLY_NESTED_TYPE4_NESTED_TYPE =
+        PACKAGE_WDEEPLY_NESTED_TYPE3;
+    static final String PACKAGE_WDEEPLY_NESTED_TYPE4_INDEX_TYPE = "PLS_INTEGER";
+    static final String PACKAGE_WDEEPLY_NESTED_TYPE5 = "TBL4";
+    static final String PACKAGE_WDEEPLY_NESTED_TYPE5_NESTED_TYPE =
+        PACKAGE_WDEEPLY_NESTED_TYPE2;
+    static final String PACKAGE_WDEEPLY_NESTED_TYPE5_INDEX_TYPE =
+        PACKAGE_WDEEPLY_NESTED_TYPE4_INDEX_TYPE;
+    static final String CREATE_PACKAGE_WDEEPLY_NESTED_TYPES =
+        CREATE_PACKAGE_PREFIX + PACKAGE_WDEEPLY_NESTED_TYPES + " AS " +
+            "\nTYPE " + PACKAGE_WDEEPLY_NESTED_TYPE1 + " IS TABLE OF " +
+                PACKAGE_WDEEPLY_NESTED_TYPE1_NESTED_TYPE + " INDEX BY " +
+                PACKAGE_WDEEPLY_NESTED_TYPE1_INDEX_TYPE + ";" +
+            "\nTYPE " + PACKAGE_WDEEPLY_NESTED_TYPE2 + " IS TABLE OF " +
+                PACKAGE_WDEEPLY_NESTED_TYPE2_NESTED_TYPE + " INDEX BY " +
+                PACKAGE_WDEEPLY_NESTED_TYPE2_INDEX_TYPE + ";" +
+            "\nTYPE " + PACKAGE_WDEEPLY_NESTED_TYPE3 + " IS RECORD (" +
+                "\n" + PACKAGE_WDEEPLY_NESTED_TYPE3_FIELD1_NAME + " " +
+                    PACKAGE_WDEEPLY_NESTED_TYPE3_FIELD1_TYPE + "," +
+                "\n" + PACKAGE_WDEEPLY_NESTED_TYPE3_FIELD2_NAME + " " +
+                    PACKAGE_WDEEPLY_NESTED_TYPE3_FIELD2_TYPE + "," +
+                "\n" + PACKAGE_WDEEPLY_NESTED_TYPE3_FIELD3_NAME + " " +
+                    PACKAGE_WDEEPLY_NESTED_TYPE3_FIELD3_TYPE +
+            "\n);" +
+            "\nTYPE " + PACKAGE_WDEEPLY_NESTED_TYPE4 + " IS TABLE OF " +
+                PACKAGE_WDEEPLY_NESTED_TYPE4_NESTED_TYPE + " INDEX BY " +
+                PACKAGE_WDEEPLY_NESTED_TYPE4_INDEX_TYPE + ";" +
+            "\nTYPE " + PACKAGE_WDEEPLY_NESTED_TYPE5 + " IS TABLE OF " +
+                PACKAGE_WDEEPLY_NESTED_TYPE5_NESTED_TYPE + " INDEX BY " +
+                PACKAGE_WDEEPLY_NESTED_TYPE5_INDEX_TYPE + ";" +
+        "\nEND " + PACKAGE_WDEEPLY_NESTED_TYPES + ";";
+    @Test
+    public void testPackageWithDeeplyNestedTypes() {
+        parser.ReInit(new StringReader(CREATE_PACKAGE_WDEEPLY_NESTED_TYPES));
+        boolean worked = true;
+        @SuppressWarnings("unused") PLSQLPackageType packageType = null;
+        try {
+            packageType = parser.parsePLSQLPackage();
+        }
+        catch (ParseException pe) {
+            worked = false;
+        }
+        assertTrue("package with deeply nested types should parse", worked);
+        assertEquals("package with deeply nested types has wrong name",
+            packageType.getPackageName(), PACKAGE_WDEEPLY_NESTED_TYPES);
+        assertNull("package with deeply nested types should have no procedures",
+            packageType.getProcedures());
+        assertNotNull("package with deeply nested types should have types",
+            packageType.getTypes());
+        assertEquals("package with deeply nested types should have exactly 5 types", 5,
+            packageType.getTypes().size());
+        PLSQLType t1 = packageType.getTypes().get(0);
+        assertEquals("package with deeply nested types type1 has wrong name",
+            PACKAGE_WDEEPLY_NESTED_TYPE1, t1.getTypeName());
+        PLSQLCollectionType collT1 = (PLSQLCollectionType)t1;
+        assertTrue("package with deeply nested types type1 nestedType should " +
+                "be associative", collT1.isIndexed());
+        DatabaseType nestedType = collT1.getNestedType();
+        assertEquals("package with deeply nested types type1 nestedType has wrong name",
+            new VarChar2Type().getTypeName(), nestedType.getTypeName());
+        assertEquals("package with deeply nested types type1 nestedType has wrong size",
+            111L, ((SizedType)nestedType).getSize());
+        PLSQLType t2 = packageType.getTypes().get(1);
+        assertEquals("package with deeply nested types type2 has wrong name",
+            PACKAGE_WDEEPLY_NESTED_TYPE2, t2.getTypeName());
+        PLSQLCollectionType collT2 = (PLSQLCollectionType)t2;
+        assertTrue("package with deeply nested types type2 nestedType should " +
+                "be associative", collT2.isIndexed());
+        nestedType = collT2.getNestedType();
+        assertEquals("package with deeply nested types type2 nestedType has wrong name",
+            new NumericType().getTypeName(), nestedType.getTypeName());
+
+        PLSQLType t3 = packageType.getTypes().get(2);
+        assertEquals("package with deeply nested types type3 has wrong name",
+            PACKAGE_WDEEPLY_NESTED_TYPE3, t3.getTypeName());
+        PLSQLRecordType t3Rec = (PLSQLRecordType)t3;
+        List<FieldType> fields = t3Rec.getFields();
+        assertEquals("package with deeply nested types type3 field1 has wrong name",
+            PACKAGE_WDEEPLY_NESTED_TYPE3_FIELD1_NAME, fields.get(0).getFieldName());
+        assertEquals("package with deeply nested types type3 field1 has wrong type",
+            PACKAGE_WDEEPLY_NESTED_TYPE3_FIELD1_TYPE, fields.get(0).getTypeName());
+        assertEquals("package with deeply nested types type3 field2 has wrong name",
+            PACKAGE_WDEEPLY_NESTED_TYPE3_FIELD2_NAME, fields.get(1).getFieldName());
+        assertEquals("package with deeply nested types type3 field2 has wrong type",
+            PACKAGE_WDEEPLY_NESTED_TYPE3_FIELD2_TYPE, fields.get(1).getTypeName());
+        assertEquals("package with deeply nested types type3 field2 has wrong name",
+            PACKAGE_WDEEPLY_NESTED_TYPE3_FIELD3_NAME, fields.get(2).getFieldName());
+        assertEquals("package with deeply nested types type3 field2 has wrong type",
+            PACKAGE_WDEEPLY_NESTED_TYPE3_FIELD3_TYPE, fields.get(2).getTypeName());
     }
 }
